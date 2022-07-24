a18578fa195195b678dd717ed9925643eb20aade
Vivekvvijayan <71512595+Vivekvvijayan@users.noreply.github.com>
2022-07-24 12:33:32 +0530

    added firstly

--- App.js
+++ App.js
@@ -1,20 +1,126 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export default function App() {
+export default App = () => {
+
+    const [todo, setTodo] = useState("");
+    const [val, setVal] = useState("");
+
+  const insertItem = async () => {
+    try {
+      await AsyncStorage.setItem('key1', todo);
+      alert('Item saved successfully');
+    } catch (e) {
+      alert('Error saving data');
+    }
+  }
+
+  const getItems = async () => {
+
+    try{
+
+        const data = await AsyncStorage.getItem('key1');
+        if(data !== null){
+            setVal(data);
+            setTodo("")
+        } else{
+          alert('No data found');
+        }
+
+    }catch(e){
+      alert('Error getting data');
+    }
+
+
+
+  }
+
+  const clear = async () => {
+    try{
+
+        await AsyncStorage.clear();
+        setVal("")
+
+    }catch(err){
+      alert('Error clearing data');
+    }
+    }
+  
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{alignItems:'center'}}>
+
+      <Text style={{ fontSize: 25,color:'#009c43' }}>Today's Task</Text>
+      <Text style={{ fontSize: 27,color:'red',marginTop:10 }}>{val}</Text>
+
+      </View>
+      <View>
+
+      <TextInput
+        placeholder='Enter Task...'
+        style={styles.input}
+        onChangeText={text => setTodo(text)}
+        value={todo}
+        />
+      <TouchableOpacity style={styles.btn}
+        onPress={() => insertItem()}
+        >
+        <Text style={styles.text}>ADD</Text>
+
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.btn}
+        onPress={() =>getItems()}
+      >
+
+        <Text style={styles.text}>GET</Text>
+
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btn}
+        onPress={() =>clear()}
+      >
+
+        <Text style={styles.text}>CLEAR</Text>
+
+      </TouchableOpacity>
+
+        </View>
     </View>
-  );
-}
+  )
+  }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
-});
+  btn: {
+    width: 200,
+    height: 40,
+    backgroundColor: '#009c43',
+    padding: 5,
+    marginTop: 10,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  text: {
+    color: '#f1f1f1',
+    fontSize: 18
+  },
+  input: {
+    width: 200,
+    height: 60,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+    padding: 5
+
+  }
+})
